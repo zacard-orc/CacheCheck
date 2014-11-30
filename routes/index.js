a73d813f626637704097e8cb17f6514b624b3d21
zacard-orc <13816025562@139.com>
2014-11-30 15:04:16 +0800

    0.0.5:New Prototype

--- routes/index.js
+++ routes/index.js
@@ -13,12 +13,37 @@ router.get('/news', function(req, res) {
 });
 
 router.get('/htable', function(req, res) {
-    res.render('htable', { title: 'HTable' });
+    var xdata=[
+        {
+            "name":"上海",
+            "os":"Windows 7",
+            "timing":
+            {
+                "t_dns":4.3,
+                "t_conn":4.5,
+                "t_send":9.8,
+                "t_wait":13.7,
+                "t_recv":39.2
+            },
+            "geo":1131
+        },
+        {
+            "name":"广州",
+            "os":"Safari",
+            "timing":
+            {
+                "t_dns":14.3,
+                "t_conn":9.5,
+                "t_send":3.8,
+                "t_wait":11.7,
+                "t_recv":8.2
+            },
+            "geo":571
+        }
+    ]
+    res.render('htable', { title: 'HTable', restxt:xdata});
 });
 
-router.get('/hv', function(req, res) {
-    res.render('hview', { });
-});
 
 router.get('/hext', function(req, res) {
     res.render('hext', {title:'Table Extend' });
