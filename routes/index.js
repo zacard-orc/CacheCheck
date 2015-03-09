616e8db4afbbeec1c8f0438ababceefcf316a36b
zarcard-orc <13816025562@139.com>
2015-03-09 22:29:19 +0800

    0.1.15:seesion test

--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,8 @@ router.get('/side',function(req,res){res.render('slider',{title:'H5 Canvas'});})
 router.get('/jt',function(req,res){res.render('jdata',{title:'H5 Canvas'});});
 router.get('/div5',function(req,res){res.render('v_div_css5',{title:'H5 Canvas'});});
 router.get('/s_circle',function(req,res){res.render('v_shape_circle',{title:'H5 Canvas'});});
+router.get('/s_fbd',function(req,res){res.render('v_shape_fbd',{title:'H5 Canvas'});});
+router.get('/s_r_rect',function(req,res){res.render('v_shape_rotate_rect',{title:'H5 Canvas'});});
 router.get('/s_star',function(req,res){res.render('v_shape_star',{title:'H5 Canvas'});});
 
 router.post('/postkk',function(req,res){
